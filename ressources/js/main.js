a5876668ab4718794b2e4dd2dca9a6428f95fecf
Unknown <damienvignes@hotmail.com>
2018-03-14 17:00:20 +0100

    ajout counter

--- ressources/js/main.js
+++ ressources/js/main.js
@@ -165,9 +165,9 @@ $(document).ready(function(){
 
 
 //Add counter
-$(document).ready(function(){
-  $('.counter-num').counterUp ({
-    delay: 10,
-    time: 2000
-  });
-});
+      $(document).ready(function($) {
+            $('.counter-num').counterUp({
+                delay: 10,
+                time: 2000
+            });
+        });
